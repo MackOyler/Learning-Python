958e057fc3d3f23c23d71ae6e24579782868b6b5
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-04-21 20:42:14 -0500

    very beginnings of player creation

--- Python-Platformer-main/tutorial.py
+++ Python-Platformer-main/tutorial.py
@@ -22,6 +22,8 @@ class Player(pygame.sprite.Sprite):
         self.x_vel = 0
         self.y_vel = 0
         self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
 
     def move(self, dx, dy):
         self.rect.x += dx
@@ -29,9 +31,21 @@ class Player(pygame.sprite.Sprite):
         
     def move_left(self, vel):
         self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
         
     def move_right(self, vel):
         self.x_vel = vel
+        if self.direction != "right":
+            self.direction = "right"
+            self.animation_count = 0
+            
+    def loop(self, fps):
+        self.move(self.x_vel, self.y_vel)
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -45,9 +59,11 @@ def get_background(name):
             
     return tiles , image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tile)
+        
+    player.draw(window)
     
     pygame.display.update()
 
@@ -55,6 +71,8 @@ def draw(window, background, bg_image):
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Blue.png")
+    
+    player = Player(100, 100, 50, 50)
     
     run = True
     while run:
@@ -65,7 +83,7 @@ def main(window):
                 run = False
                 break
             
-        draw(window, background, bg_image)
+        draw(window, background, bg_image, player)
         
     pygame.quit()
     quit()
