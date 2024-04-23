727cef34cf301bf915cb3110646e27ee91d2d537
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-04-23 08:53:59 -0500

    fixed typo; L124

--- Python-Platformer-main/tutorial.py
+++ Python-Platformer-main/tutorial.py
@@ -121,7 +121,7 @@ class Object(pygame.sprite.Sprite):
 class Block(Object):
     def __init__(self, x, y, size):
         super().__init__(x, y, size, size)
-        block = load_block(size)
+        block = get_block(size)
         self.image.blit(block, (0, 0))
         self.mask = pygame.mask.from_surface(self.image)
 
