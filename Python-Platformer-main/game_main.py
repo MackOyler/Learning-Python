f2493628cd978eaa09e149ec84f3d31c67739eb8
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-04-23 11:07:54 -0500

    added scrolling

--- Python-Platformer-main/game_main.py
+++ Python-Platformer-main/game_main.py
@@ -128,8 +128,8 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.sprite.get_rect(topleft=(self.rect.x, self.rect.y))
         self.mask = pygame.mask.from_surface(self.sprite)
     
-    def draw(self, win):
-        win.blit(self.sprite, (self.rect.x, self.rect.y))
+    def draw(self, win, offset_x):
+        win.blit(self.sprite, (self.rect.x - offset_x, self.rect.y))
         
 class Object(pygame.sprite.Sprite):
     def __init__(self, x, y, width, height, name=None):
@@ -140,8 +140,8 @@ class Object(pygame.sprite.Sprite):
         self.height = height
         self.name = name
         
-    def draw(self, win):
-        win.blit(self.image, (self.rect.x, self.rect.y))
+    def draw(self, win, offset_x):
+        win.blit(self.image, (self.rect.x - offset_x, self.rect.y))
         
 class Block(Object):
     def __init__(self, x, y, size):
@@ -162,14 +162,14 @@ def get_background(name):
             
     return tiles , image
 
-def draw(window, background, bg_image, player, objects):
+def draw(window, background, bg_image, player, objects, offset_x):
     for tile in background:
         window.blit(bg_image, tile)
         
     for obj in objects:
-        obj.draw(window)    
+        obj.draw(window, offset_x)    
         
-    player.draw(window)
+    player.draw(window, offset_x)
     
     pygame.display.update()
     
@@ -230,9 +230,10 @@ def main(window):
             
         player.loop(FPS)
         handle_move(player, floor)    
-        draw(window, background, bg_image, player, floor)
+        draw(window, background, bg_image, player, floor, offset_x)
         
-        if (player.rect.right - offset_x >= WIDTH - scroll_area_width and player.x_vel > 0):
+        if ((player.rect.right - offset_x >= WIDTH - scroll_area_width) and player.x_vel > 0) or (
+                (player.rect.left - offset_x <= scroll_area_width) and player.x_vel < 0):
             offset_x += player.x_vel
         
     pygame.quit()
