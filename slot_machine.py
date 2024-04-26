7dc3da20c391aa80c19f1934c64edcef5f0dd18b
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-04-26 09:50:15 -0500

    started sm

---
+++ slot_machine.py
@@ -0,0 +1,22 @@
+
+
+def deposit():
+    while True:
+        amount = input("Enter the amount you would like to deposit: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            print("Please enter a number.")
+            
+    return amount 
+
+
+
+def main():
+    balance = deposit()
+    
+main()
