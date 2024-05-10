7f18df8509680042dfac4eb8c2116b7118700e0a
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-10 11:38:33 -0500

    prog on wpm

--- wpm/typewpm.py
+++ wpm/typewpm.py
@@ -3,10 +3,39 @@ from curses import wrapper
 
 def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Welcome to WPM")
+    stdscr.addstr("Welcome to WPM Speed Typing Test!")
     stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
     stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "This is a test; this is testing text for this app, and I hope you studied."
+    current_text = []
+    
+    
+    while True:     
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+            
+        stdscr.refresh()
+        
+        key = stdscr.getkey()
+        
+        if ord(key) == 27:
+            break
+        
+        if key in ("KEY_BACKSPACE", '\b', "\x7f")
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+                
+        
+        current_text.append(key)
+
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
@@ -14,5 +43,6 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
+    wpm_test(stdscr)
 
 wrapper(main)
