8ad67e62cf0658b1fb2725476398d0dffd38ab28
Mack Oyler <120589371+MackOyler@users.noreply.github.com>
2024-05-10 11:17:50 -0500

    wpm work

--- wpm/typewpm.py
+++ wpm/typewpm.py
@@ -1,15 +1,18 @@
 import curses
 from curses import wrapper
 
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to WPM")
+    stdscr.addstr("\nPress any key to begin!")
+    stdscr.refresh()
+    stdscr.getkey()
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
-    stdscr.clear()
-    stdscr.addstr(0, 0, "Hello,Clarice")
-    stdscr.refresh()
-    key = stdscr.getkey()
-    print(key)
+    start_screen(stdscr)
 
 wrapper(main)
